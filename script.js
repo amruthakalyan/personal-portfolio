bb079a9eb2a40343e5e43a8550e1151aff812059
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2024-03-26 21:18:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,46 @@ dark.addEventListener('click',()=>{
    }
     typeWriter();
 
+//Slideshow  imgs
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slideIndex++; // Increment slideIndex
+  
+  // If slideIndex is greater than the number of slides, reset it to 1
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  
+  // Display the current slide
+  slides[slideIndex - 1].style.display = "block";
+
+  // Update the dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  dots[slideIndex - 1].className += " active";
+  
+  // Call showSlides() again after 2 seconds
+  setTimeout(showSlides, 5000);
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
