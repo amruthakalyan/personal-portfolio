7075b8a429969eac6869b090ca31e75c8c9cbc2a
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2025-05-21 22:55:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.body.style.backgroundColor = 'black',
 document.body.style.color = 'white';
-// let light = document.getElementById('light');
-// let dark= document.getElementById('dark');
-// light.addEventListener('click',()=>{
-//     document.body.style.backgroundColor = "white";
-//     document.body.style.color = "black";
-// })
-// dark.addEventListener('click',()=>{
-//     document.body.style.backgroundColor = "black";
-//     document.body.style.color = "white";
+let light = document.getElementById('light');
+let dark= document.getElementById('dark');
+light.addEventListener('click',()=>{
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+})
+dark.addEventListener('click',()=>{
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
 // })
 
 
